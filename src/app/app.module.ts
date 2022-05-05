65282ff0252c69105a6e09e61acdb73b9743489f
Cristhian Bastidas <cristhian@udenar.edu.co>
2022-05-04 22:17:37 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardsResultadosComponent } from './components/cards-resultados/cards-resultados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CardsResultadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
